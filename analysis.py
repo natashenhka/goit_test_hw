166122efe697536a605c98969d6491027951e1cd
natashenhka <muzitskaya@gmail.com>
2026-01-05 18:16:13 +0200

    update sales value for nadia

--- analysis.py
+++ analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
-data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 900, 500]}
+data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [12000, 9000, 5000]}
 df = pd.DataFrame(data)
 print("Продажі по містах:")
 print(df)
